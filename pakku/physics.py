d7061987cb712c2e93041dfa52d55633e109ec86
Lucas <lucasdekam@gmail.com>
2024-11-01 22:48:32 +0100

    remove commented code

--- pakku/physics.py
+++ pakku/physics.py
@@ -6,27 +6,6 @@ Simple physics utilities, such as computing the density or dipole orientation ve
 from scipy import constants
 
 
-# def water_cos_theta(
-#     o_pos: np.ndarray,
-#     h1_pos: np.ndarray,
-#     h2_pos: np.ndarray,
-#     axis: int,
-#     box: np.ndarray,
-# ):
-#     """
-#     Calculate cos theta for a collection of water molecules, given the
-#     positions of the individual atoms
-#     """
-#     # Compute bond vector
-#     bond_vec_1 = minimize_vectors(h1_pos - o_pos, box=box)
-#     bond_vec_2 = minimize_vectors(h2_pos - o_pos, box=box)
-#     dipole_vector = bond_vec_1 + bond_vec_2
-
-#     # Project dipole vector on main axis
-#     cos_theta = (dipole_vector[:, axis]) / np.linalg.norm(dipole_vector, axis=-1)
-#     return cos_theta
-
-
 def density(n, v, mol_mass: float):
     """
     calculate density (g/cm^3) from the number of particles
